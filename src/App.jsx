941c57d3dbbfa7465034ef82c157722c6bfc5a7c
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-10-24 14:46:11 +0300

    styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <Button>Deneme Butonu</Button>
+      <Input type="number" placeholder="Number of guests" />
+    </>
+  );
 }
 
 export default App;
