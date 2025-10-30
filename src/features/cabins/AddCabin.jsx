34f5bc432a28a7975d6d098943ae90909ce85ad3
Ahmet Tuna POTUR <tpotur@gmail.com>
2025-10-30 12:17:02 +0300

    367 Building a Modal Window using a REACT PORTAL

---
+++ src/features/cabins/AddCabin.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import Button from "../../ui/Button";
+import CreateCabinForm from "./CreateCabinForm";
+import Modal from "../../ui/Modal";
+
+function AddCabin() {
+  const [isOpenModel, setIsOpenModel] = useState(false);
+  return (
+    <div>
+      <Button onClick={() => setIsOpenModel((show) => !show)}>
+        Add new cabin
+      </Button>
+      {isOpenModel && (
+        <Modal onClose={() => setIsOpenModel(false)}>
+          <CreateCabinForm onCloseModal={() => setIsOpenModel(false)} />
+        </Modal>
+      )}
+    </div>
+  );
+}
+
+export default AddCabin;
